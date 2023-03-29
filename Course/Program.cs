697dd8dc8a5ad0e2daef3d55a47990a9daa5d037
Vitor Bahir <vitorb82014@gmail.com>
2023-03-29 20:44:11 -0300

    Initial implementation

---
+++ Course/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Console.Write("Enter file full path: ");
+        string path = Console.ReadLine();
+        try
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] votingRecord = sr.ReadLine().Split(',');
+                    string candidate = votingRecord[0];
+                    int votes = int.Parse(votingRecord[1]);
+
+                    if (map.ContainsKey(candidate))
+                    {
+                        map[candidate] += votes;
+                    }
+                    else
+                    {
+                        map[candidate] = votes;
+                    }
+
+                }
+            }
+
+            foreach (var mapp in map)
+            {
+                Console.WriteLine(mapp.Key + ": " + mapp.Value);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
